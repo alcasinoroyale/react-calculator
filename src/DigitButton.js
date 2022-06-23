7f1d59f746a8b36fafd2822c5e906cc56d1db6bb
alcasinoroyale <alexzdatny@gmail.com>
2022-06-22 22:11:14 -0400

    Setup evaluate function

--- src/DigitButton.js
+++ src/DigitButton.js
@@ -2,9 +2,10 @@ import { ACTIONS } from "./App"
 
 export default function DigitButton({ dispatch, digit }) {
   return (
-
-  <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, PAYLOAD: { digit } })}>
-    {digit}
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
     </button>
   )
 }
