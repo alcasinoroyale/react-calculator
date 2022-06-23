355ae73cb9af69cc3d748eb86620492bf639a0a0
alcasinoroyale <alexzdatny@gmail.com>
2022-06-22 22:35:53 -0400

    Complete calculator actions

--- src/App.js
+++ src/App.js
@@ -14,6 +14,13 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch(type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        }
+      }
       if (payload.digit === "0" && state.currentOperand === "0") {
         return state
       }
@@ -28,6 +35,13 @@ function reducer(state, { type, payload }) {
         if (state.currentOperand == null && state.previousOperand == null) {
           return state
         }
+        if (state.currentOperand == null) {
+          return {
+            ...state,
+            operation: payload.operation,
+          }
+        }
+
         if (state.previousOperand == null) {
           return {
             ...state,
@@ -43,7 +57,37 @@ function reducer(state, { type, payload }) {
           currentOperand: null
         }
       case ACTIONS.CLEAR:
-      return {}
+        return {}
+      case ACTIONS.DELETE_DIGIT:
+        if (state.overwrite) {
+          return {
+            ...state,
+            overwrite: false,
+            currentOperand: null,
+          }
+        }
+        if (state.currentOperand == null) return state
+        if (state.currentOperand.length === 1) {
+          return { ...state, currentOperand: null }
+          }
+
+          return {
+            ...state, currentOperand: state.currentOperand.slice(0, -1)
+          }
+      case ACTIONS.EVALUATE:
+        if (
+          state.operation == null ||
+          state.currentOperand == null ||
+          state.previousOperand == null
+        ) {
+          return state
+        }
+        return {
+          ...state,
+          previousOperand: null,
+          operation: null,
+          currentOperand: evaluate(state),
+        }
   }
 }
 
@@ -54,17 +98,30 @@ function evaluate({ currentOperand, previousOperand, operation}) {
   let computation = ""
   switch (operation) {
     case "+":
-    computation = prev + current
-    break
+      computation = prev + current
+      break
     case "-":
-    computation = prev - current
+      computation = prev - current
+      break
     case "*":
-    computation = prev * current
+      computation = prev * current
+      break
     case "÷":
-    computation = prev / current
-    break
+      computation = prev / current
+      break
   }
   return computation.toString()
+}
+
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+})
+
+function formatOperand(operand) {
+  if (operand == null) return
+  const [integer, decimal] = operand.split('.')
+  if (decimal == null) return INTEGER_FORMATTER.format(integer)
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
 }
 
 function App() {
@@ -77,11 +134,11 @@ function App() {
     <div className="calculator-grid">
     <div className="output">
       <div className="previous-operand">{previousOperand} {operation}</div>
-      <div className="current-operand">{currentOperand}</div>
+      <div className="current-operand">{formatOperand(currentOperand)}</div>
     </div>
       <button className="span-two"
       onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
-      <button>DEL</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
       <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
@@ -97,7 +154,8 @@ function App() {
       <OperationButton operation="-" dispatch={dispatch} />
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
-      <button className="span-two">=</button>
+      <button className="span-two"
+      onClick={() => dispatch({ type: ACTIONS.EVALUATE })}>=</button>
     </div>
   );
 }
